4a82e70b932d2d66b909397b4dd6e9e70d941193
GabrielMcryu <gabrielmcryu@gmail.com>
2025-04-01 17:42:01 +0300

    read file contents

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    contents = get_book_text('./books/frankenstein.txt')
+    print(contents)
+
+main()
