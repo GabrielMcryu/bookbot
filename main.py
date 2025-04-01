59ac77e225289d0fc3feac2a5b6af6a9b89caf8d
GabrielMcryu <gabrielmcryu@gmail.com>
2025-04-01 20:39:39 +0300

    can print all data of counts from txt

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from stats import get_num_words
+from stats import sort_dict
 
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
-        character_count = get_num_words(file_contents)
-        return character_count
+        character_count, total_words = get_num_words(file_contents)
+        dict_list = sort_dict(character_count)
+        return dict_list, total_words
 
 
 def main():
-    character_count = get_book_text('./books/frankenstein.txt')
-    print(character_count)
+    dict_list, total_words = get_book_text("./books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for dict in dict_list:
+        if dict["name"].isalpha():
+            print(f"{dict["name"]}: {dict["num"]}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -8,8 +8,26 @@ def character_counts(all_words):
                 character_count[character.lower()] = 1
     return character_count
 
+def sort_on(dict):
+    return dict["num"]
+
 def get_num_words(file_contents):
     all_words = file_contents.split()
+    total_words = len(all_words)
     character_count = character_counts(all_words)
-    return character_count
+    return character_count, total_words
+
+def sort_dict(character_count):
+    dict_list = []
+    for key in character_count:
+        temp_dict = {}
+        temp_dict["name"] = key
+        temp_dict["num"] = character_count[key]
+        dict_list.append(temp_dict)
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+
+
+
 
